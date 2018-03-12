a34d1edb84b87c9f739693729ef592788dcd0215
gaurav94 <agrawal.gaurav94@gmail.com>
2018-03-12 11:14:10 -0400

    Second commit

---
+++ Scripts/AfTree.js
@@ -0,0 +1,90 @@
+﻿
+/*
+////Please enter tree's root element's WebID address.
+var RootElement = 'https://njnwkaps326v/piwebapi/assetdatabases/D0DL80eV7eA0SMKofC0iIj1AEk6H66MvRE-rNoYuQG5yrwUEktUUEtQUYuUFNFRy5DT01cVFJBSU5JTkdfREFUQUJBU0U/elements';
+//var RootElement = 'https://njnwkaps326v/piwebapi/assetdatabases/D0DL80eV7eA0SMKofC0iIj1AEk6H66MvRE-rNoYuQG5yrwUEktUUEtQUYuUFNFRy5DT01cVFJBSU5JTkdfREFUQUJBU0U';
+//Display path and CurrentElement
+var DisplayPath = 'CurrentElement=';
+var childrenMap = {
+    Elements: ['Elements']
+};
+
+function MakeAjaxRequest(type, url, SuccessCallBack, data) {
+    $.ajax({
+        type: type,
+        url: url,
+        cache: false,
+        async: true,
+        data: data,
+        contentType: "application/json",
+        success: SuccessCallBack
+    });
+}
+
+function node(name, type, links, parentDiv, path) {
+    this.type = type;
+    this.links = links;
+    this.flipper = $('<span class="flipper">+</span>').click(flip.bind(this, this));     
+    parentDiv.append(this.flipper).append('<span class=" + type + "><a href="'+DisplayPath + path  + '" target="F2">' + name + '</a></span><br />');
+    this.div = $('<div></div>').hide().appendTo(parentDiv);
+}
+
+        
+function loadChildren(n) {
+    n.loaded = true;
+    childrenMap[n.type].forEach(function(childCollection) {
+        $.get(n.links[childCollection], function(collection) {
+            n[childCollection] = collection.Items.map(function (item) {
+                return new node(item.Name, childCollection, item.Links, n.div, item.Path);
+            });
+        });
+    });
+}
+function flip(n) {
+    if (!n.loaded) { loadChildren(n); }
+    n.flipper.html(n.flipper.html() == '+' ? '-' : '+');
+    n.div.toggle();
+}
+$(function() {
+    root = new node('Elements', 'Elements',
+ { Elements: RootElement }, $("#root"));
+});
+
+
+
+
+//$(document).ready(function () {
+//    alert("Ready");
+
+//    $("#t1").click(function () {
+//        alert("Trans 1 called");
+
+//        $.getJSON("https://njnwkaps326v/piwebapi/streams/A0EDL80eV7eA0SMKofC0iIj1Adq3lMnFx5xGA_gBQVqBPZwEzGOwOeO1F4p-4GfUd1LRAUEktUUEtQUYuUFNFRy5DT01cVFJBSU5JTkdfREFUQUJBU0VcRFVWQUwgQU5BTFlTSVNcVFJBTlNGT1JNRVIxfEMySDQ/value", null, function (data) {document.getElementById("methane").innerHTML = data.Value})
+//        $.getJSON("https://njnwkaps326v/piwebapi/streams/A0EDL80eV7eA0SMKofC0iIj1Adq3lMnFx5xGA_gBQVqBPZwEzGOwOeO1F4p-4GfUd1LRAUEktUUEtQUYuUFNFRy5DT01cVFJBSU5JTkdfREFUQUJBU0VcRFVWQUwgQU5BTFlTSVNcVFJBTlNGT1JNRVIxfEMySDQ/value", null, function (data) { document.getElementById("ethane").innerHTML = data.Value })
+//        $.getJSON("https://njnwkaps326v/piwebapi/streams/A0EDL80eV7eA0SMKofC0iIj1Adq3lMnFx5xGA_gBQVqBPZwEzGOwOeO1F4p-4GfUd1LRAUEktUUEtQUYuUFNFRy5DT01cVFJBSU5JTkdfREFUQUJBU0VcRFVWQUwgQU5BTFlTSVNcVFJBTlNGT1JNRVIxfEMySDQ/value", null, function (data) { document.getElementById("acety").innerHTML = data.Value })
+//    });
+
+//    $("#t2").click(function () {
+//        alert("Trans 2 called");
+
+//        $.getJSON("https://njnwkaps326v/piwebapi/streams/A0EDL80eV7eA0SMKofC0iIj1AnNXewJNx5xGA_gBQVqBPZwqw_NBiZu71E2WtTxYBSN3QUEktUUEtQUYuUFNFRy5DT01cVFJBSU5JTkdfREFUQUJBU0VcRFVWQUwgQU5BTFlTSVNcVFJBTlNGT1JNRVIyfEMySDI/value", null, function (data) { document.getElementById("methane").innerHTML = data.Value })
+//        $.getJSON("https://njnwkaps326v/piwebapi/streams/A0EDL80eV7eA0SMKofC0iIj1Adq3lMnFx5xGA_gBQVqBPZwEzGOwOeO1F4p-4GfUd1LRAUEktUUEtQUYuUFNFRy5DT01cVFJBSU5JTkdfREFUQUJBU0VcRFVWQUwgQU5BTFlTSVNcVFJBTlNGT1JNRVIxfEMySDQ/value", null, function (data) { document.getElementById("ethane").innerHTML = data.Value })
+//        $.getJSON("https://njnwkaps326v/piwebapi/streams/A0EDL80eV7eA0SMKofC0iIj1Adq3lMnFx5xGA_gBQVqBPZwEzGOwOeO1F4p-4GfUd1LRAUEktUUEtQUYuUFNFRy5DT01cVFJBSU5JTkdfREFUQUJBU0VcRFVWQUwgQU5BTFlTSVNcVFJBTlNGT1JNRVIxfEMySDQ/value", null, function (data) { document.getElementById("acety").innerHTML = data.Value })
+
+//        var result = document.getElementById('methane').innerText + document.getElementById('ethane').innerText;
+
+//        document.getElementById('total').innerHTML = result;
+
+      
+//    });
+
+     
+//});
+
+
+
+
+
+
+
+*/
---
+++ Scripts/script.js
@@ -0,0 +1,84 @@
+﻿/***************************************************************************
+   Copyright 2015 OSIsoft, LLC.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ ***************************************************************************/
+
+// Define the names and object types of the AF hierarchy
+var DisplayPath;
+var childrenMap = {
+    PISystems: ['AssetServers'],
+    AssetServers: ['Databases'],
+    Databases: ['Elements'],
+    Elements: ['Elements', 'Attributes'],
+    Attributes: ['Attributes']
+};
+
+// Get base64-encoded string for basic authentication
+var makeBasicAuth = function (user, password) {
+    var tok = user + ':' + password;
+    var hash = window.btoa(tok);
+    return "Basic " + hash;
+};
+
+// Constructor for the node object
+function node(name, type, links, parentDiv, path) {
+    this.type = type;
+    this.links = links;
+    this.flipper = $('<span class="flipper">+</span>').click(flip.bind(this, this));
+   // parentDiv.append(this.flipper).append('<span class="' + type + '"> ' + name + '</span><br />');
+    parentDiv.append(this.flipper).append('<span class=" + type + "><a href="' + path + '" target="F2">' + name + '</a></span><br />');
+    this.div = $('<div></div>').hide().appendTo(parentDiv);
+    
+}
+
+// Make ajax calls to PI Web API server to get the children of each node
+function loadChildren(n) {
+    n.loaded = true;
+    childrenMap[n.type].forEach(function (childCollection) {
+        $.ajax({
+            url: n.links[childCollection],
+            success: function (collection) {
+                n[childCollection] = collection.Items.map(function (item) {
+                    return new node(item.Name, childCollection, item.Links, n.div);
+                })
+            },
+            error: function (xhr) {
+                console.log(xhr.responseText);
+            },
+            //beforeSend: function (xhr) {
+             //   xhr.setRequestHeader('Authorization', makeBasicAuth('username', 'password'));
+           // }
+        })
+    });
+}
+
+// Load children, toggle the flipper icon and show/hide the loaded element
+function flip(n) {
+    if (!n.loaded) { loadChildren(n); }
+    n.flipper.html(n.flipper.html() == '+' ? '-' : '+');
+    n.div.toggle();
+}
+
+// Add the root node when the DOM is fully loaded
+//$(function () {
+//    root = new node('PI Systems', 'PISystems',
+ //     { AssetServers: 'https://njnwkaps326v.enterprise.pseg.com/piwebapi/assetservers' }, $("#root"));
+//});
+
+$(function () {
+    root = new node('CMMS Asset Management', 'Elements',
+      { Elements: 'https://njnwkaps326v.enterprise.pseg.com/piwebapi/assetdatabases/D0DL80eV7eA0SMKofC0iIj1AEk6H66MvRE-rNoYuQG5yrwUEktUUEtQUYuUFNFRy5DT01cVFJBSU5JTkdfREFUQUJBU0U/elements' }, $("#root"));
+});
+
+
